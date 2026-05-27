bebcf2dbfa6174701f5f1f3f2036fbb36fea2b68
joascr7 <contatojoasvieiral@email.com>
2026-05-27 14:41:19 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {};
-
+const nextConfig = {
+  // Isso força o Next.js a sempre tentar carregar a partir da raiz
+  output: 'standalone', 
+};
 export default nextConfig;
