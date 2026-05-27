3bed946987e9b353239c030a93b330a651f9e15b
joascr7 <contatojoasvieiral@email.com>
2026-05-27 00:09:40 -0300

    update

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+// Usamos as variáveis de ambiente, mas se não existirem (build), 
+// atribuímos uma string vazia para não interromper o processo.
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('As variáveis de ambiente do Supabase não foram carregadas.');
-}
-
+// A inicialização agora é segura para o build
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
