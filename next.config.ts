a91c85708d2de07c80bcf0a82b8135e6bf5ca320
joascr7 <contatojoasvieiral@email.com>
2026-05-28 22:57:27 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,17 @@
-const nextConfig = {
-  // Isso força o Next.js a sempre tentar carregar a partir da raiz
-  output: 'standalone', 
-};
-export default nextConfig;
+/** @type {import('next').NextConfig} */
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development", // Só ativa em produção
+  disable: process.env.NODE_ENV === "development",
   register: true,
 });
 
-module.exports = withPWA({
-  // suas outras configurações aqui
-});
+const nextConfig = {
+  output: 'standalone', // Mantém sua configuração de servidor
+  // Adicione outras configurações aqui, se precisar
+};
+
+module.exports = withPWA(nextConfig);
