bd6222057dab9737e081ec798f6155d10afe0682
joascr7 <contatojoasvieiral@email.com>
2026-05-28 22:33:04 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -3,3 +3,16 @@ const nextConfig = {
   output: 'standalone', 
 };
 export default nextConfig;
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development", // Só ativa em produção
+  register: true,
+});
+
+module.exports = withPWA({
+  // suas outras configurações aqui
+});
