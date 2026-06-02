8ce56bd6056fc54b569405698bdb7130f7f9476e
joascr7 <contatojoasvieiral@email.com>
2026-06-02 18:48:59 -0300

    update pro

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -2,10 +2,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY!;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 if (!supabaseServiceKey) {
-  throw new Error("SUPABASE_SERVICE_KEY não está definida nas variáveis de ambiente da Vercel.");
+  throw new Error("SUPABASE_SERVICE_ROLE_KEY não está definida nas variáveis de ambiente da Vercel.");
 }
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
