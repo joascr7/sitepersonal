857fc69e06ef0e830c57951f3bce95b321115443
Joás <152432003+joascr7@users.noreply.github.com>
2026-06-10 16:21:38 -0300

    Atualizar o supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,5 +3,13 @@ import { createBrowserClient } from '@supabase/ssr';
 // Esta instância é EXCLUSIVA para o lado do cliente (Componentes 'use client')
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    cookieOptions: {
+      maxAge: 31536000, // Força a validade para 1 ano (31.536.000 segundos)
+      path: '/',
+      sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production', // true em produção, false em localhost
+    }
+  }
 );
